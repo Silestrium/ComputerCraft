cb98c3c8bef5340ccd0a7e1473a3b4de5efef279
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 19:19:32 +0100

    Create app2.lua

---
+++ app2/app2.lua
@@ -0,0 +1,24 @@
+--APP2 Auslesen und an den Hauptcomputer senden
+ 
+intIdMainComputer = 2 -- Die ID des Empfangenden Main Computers
+intIdTablet = 4 -- Die ID des Empfangenden Tablets
+ 
+p=peripheral.wrap("left") --Angeben an welcher Seite der Block angeschlossen ist
+rednet.open("back") --Aktieviert das Modem das hinten angeschlossen ist
+ 
+while true do --Always loop
+ 
+    intMaxFluidStorage = p.getAvailableFluidStorage() -- Gibt aus wie viel Fluid Storage im ME System ist
+    intUsedFluidStorage = p.getUsedFluidStorage() --Gibt aus wie viel Platz des Fluid Storage verbraucht ist
+ 
+    intMaxItemStorage = p.getAvailableItemStorage() -- Gibt aus wie viel Item Storage im ME System ist
+    intUsedItemStorage = p.getUsedItemStorage() --Gibt aus wie viel Platz des Item Storage verbraucht ist
+ 
+    sendmessage = {KeyItemsMax = intMaxItemStorage , KeyItemsUsed = intUsedItemStorage , KeyFluidMax = intMaxFluidStorage , KeyFluidUsed = intUsedFluidStorage }
+    
+    rednet.send(intIdMainComputer,sendmessage)
+    rednet.send(intIdTablet,sendmessage)
+ 
+    sleep(20)
+ 
+end

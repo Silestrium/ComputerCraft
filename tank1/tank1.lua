cae0123a03ae3e8ff4986f1704288d5bdb0dd7b7
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 20:55:35 +0100

    tank send rednet

--- tank1/tank1.lua
+++ tank1/tank1.lua
@@ -1,11 +1,25 @@
---
+-- Tank 1 PC
+
+intIdMainComputer = 2 -- Die ID des Empfangenden Main Computers
+intIdTablet = 4 -- Die ID des Empfangenden Tablets
 
 p_left=peripheral.wrap("left") --Angeben an welcher Seite der Block angeschlossen ist
 p_right=peripheral.wrap("right") --Angeben an welcher Seite der Block angeschlossen ist
 rednet.open("back") --Aktieviert das Modem das hinten angeschlossen ist
 
-leftTankCap = p_left.getTankCapacity()
-leftTankStored = p_left.getStored()
-leftTankFilledPercent = p_left.getFilledPercentage()
-
-print(leftTankCap , "Filled" , leftTankFilledPercent , "%")
+while true do --Always loop
+ 
+    leftTankCap = p_left.getTankCapacity()
+    leftTankStored = p_left.getStored()
+    leftTankFilledPercent = p_left.getFilledPercentage()
+    
+    print(leftTankCap , "Filled" , leftTankFilledPercent , "%")
+ 
+    sendmessage = {KeyLeftTankCap = leftTankCap , KeyLeftTankStored = leftTankStored , KeyLeftTankFillPercent = leftTankFilledPercent }
+    
+    rednet.send(intIdMainComputer,sendmessage)
+    rednet.send(intIdTablet,sendmessage)
+ 
+    sleep(20)
+ 
+end

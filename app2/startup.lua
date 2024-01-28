959667cc15b7f56939e07c8f3ee8a86ed116d12c
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 21:35:02 +0100

    startup script auf dll link geändert

--- app2/startup.lua
+++ app2/startup.lua
@@ -1,3 +1,3 @@
 shell.run("delete mesystem.lua")
-shell.run("wget https://raw.githubusercontent.com/Silestrium/ComputerCraft/main/app2/mesystem.lua mesystem.lua")
+shell.run("wget https://github.com/Silestrium/ComputerCraft/blob/main/app2/mesystem.lua mesystem.lua")
 shell.run("mesystem.lua")

2deb9615b802131d7dd143d768053b6f4ceae800
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 19:53:11 +0100

    Update tank1.lua

--- tank1/tank1.lua
+++ tank1/tank1.lua
@@ -1,1 +1,9 @@
 --
+
+p_left=peripheral.wrap("left") --Angeben an welcher Seite der Block angeschlossen ist
+p_right=peripheral.wrap("right") --Angeben an welcher Seite der Block angeschlossen ist
+rednet.open("back") --Aktieviert das Modem das hinten angeschlossen ist
+
+leftTankCap = p_left.getTankCapacity()
+
+print(leftTankCap)

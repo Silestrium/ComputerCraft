2accf5d20be8033bab5e6ce789f7478c754db516
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 18:52:59 +0100

    Update startup.lua

--- mainsystem/startup.lua
+++ mainsystem/startup.lua
@@ -1,3 +1,3 @@
 delete mainsystem.lua
-wget http://192.168.178.36/Minecraft/Hauptsystem/mainsystem.lua mainsystem.lua
-shell.run("Hauptsystem.lua")
+wget https://github.com/Silestrium/ComputerCraft/blob/main/mainsystem/mainsystem.lua mainsystem.lua
+shell.run("mainsystem.lua")

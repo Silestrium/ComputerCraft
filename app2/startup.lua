954aed406f74e9b6b2a33aed107bb04d9ab4f88d
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 19:24:28 +0100

    Update startup.lua

--- app2/startup.lua
+++ app2/startup.lua
@@ -1,3 +1,3 @@
 shell.run("delete app2.lua")
-shell.run("wget https://raw.githubusercontent.com/Silestrium/ComputerCraft/main/mainsystem/app2.lua app2.lua")
+shell.run("wget https://raw.githubusercontent.com/Silestrium/ComputerCraft/main/app2/app2.lua app2.lua")
 shell.run("app2.lua")

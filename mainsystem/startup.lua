d13d729b92b85b8a5c973ce8926dd2b61c4d4933
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-28 19:16:30 +0100

    Update startup.lua

--- mainsystem/startup.lua
+++ mainsystem/startup.lua
@@ -1,3 +1,3 @@
-delete mainsystem.lua
-wget https://raw.githubusercontent.com/Silestrium/ComputerCraft/main/mainsystem/mainsystem.lua mainsystem.lua
+shell.run("delete mainsystem.lua")
+shell.run("wget https://raw.githubusercontent.com/Silestrium/ComputerCraft/main/mainsystem/mainsystem.lua mainsystem.lua")
 shell.run("mainsystem.lua")

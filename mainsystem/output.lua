bbe0e89fec8d728b2ff1e3621eea538b5da20ecb
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-29 18:43:28 +0100

    readfile test

--- mainsystem/output.lua
+++ mainsystem/output.lua
@@ -7,6 +7,20 @@ while true do
 
     file = fs.open("mesystem.txt","r")
     itemsUsed = file.readLine()
+
+    local file = fs.open('your/file/path')
+    local data = fs.readAll()
+    file.close()
+    
+    local t = {}
+    for v in data:gmatch('(.-)n') do
+      table.insert(t,v)
+    end
+    return t
+
+    print(t)
+  
+
     file.close()
 
     if IntID == intIDAusgabePC then -- Ausgabe auf PC

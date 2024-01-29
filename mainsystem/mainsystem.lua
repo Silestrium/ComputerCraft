091b8c2b0acc1f417a7e791178862ff78cc23a32
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-29 14:13:01 +0100

    -

--- mainsystem/mainsystem.lua
+++ mainsystem/mainsystem.lua
@@ -1,7 +1,7 @@
 
 
 local newTabID = multishell.launch({}, "output.lua")
-multishell.setTitle(newTabID, "ms_mesystem")
+multishell.setTitle(newTabID, "output")
 
 local newTab2 = multishell.launch({}, "ms_mesystem.lua")
 multishell.setTitle(newTab2, "ms_mesystem")

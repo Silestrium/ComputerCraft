6bd6627cdadc1b65d76d319550d66283a093383f
Silestrium <39763666+Silestrium@users.noreply.github.com>
2024-01-29 14:07:17 +0100

    datei öffnen lesen hinzu und while loop

--- mainsystem/output.lua
+++ mainsystem/output.lua
@@ -2,17 +2,25 @@ intIDAusgabePC = 2
 
 local monitor = peripheral.wrap( "top" )
 
+file = fs.open("mesystem.txt","r")
+itemsUsed = file.readLine()
+file.close()
 
+while true do
 
-if IntID == intIDAusgabePC then -- Ausgabe auf PC
-        
-    term.redirect(monitor)
-    print("Me System Status:")
-    print("Items", itemsUsed , " / " , itemsMax)
-    print("Fluids:",  fluidsUsed , "/" , fluidsMax)
-    print("Mekanism Tanks:")
-    print("Left Tank Stored:", leftTankStored, leftTankFilledPercent, "%")
-    -- All the prints you want here
-    -- term.restore()
+    if IntID == intIDAusgabePC then -- Ausgabe auf PC
+            
+        term.redirect(monitor)
+        print("Me System Status:")
+        print("Items", itemsUsed , " / " , itemsMax)
+        print("Fluids:",  fluidsUsed , "/" , fluidsMax)
+        print("Mekanism Tanks:")
+        print("Left Tank Stored:", leftTankStored, leftTankFilledPercent, "%")
+        -- All the prints you want here
+        -- term.restore()
+
+    end
+
+    sleep(5)
 
 end
